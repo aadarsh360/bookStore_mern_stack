6e5462315146d37fba1e89357239bff0250cfcbf
aadarsh360 <aadarshpandey64@gmail.com>
2025-02-11 13:00:56 +0530

    create our first http route

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,12 @@ const PORT = require('./config');
 
 const app = express();
 
+// first http get routes 
+app.get('/', (request, response)=>{
+    console.log(request);
+    return response.status(234).send("Welcome To MERN Stack Project");
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`Server running at port:${PORT}`);
